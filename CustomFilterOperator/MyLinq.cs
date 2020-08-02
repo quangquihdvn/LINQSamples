8f1be15e33a56f6eed3b31f5d88d59534bdc5d89
Bùi Quang Quí <quangquihdvn@gmail.com>
2020-08-02 11:20:07 +0700

    update source 2.8.2020

---
+++ CustomFilterOperator/MyLinq.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomFilterOperator
+{
+    public static class MyLinq
+    {
+        //1. Read only
+        //2. Forward only
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source,Func<T,bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+                
+            }
+        }
+    }
+}
